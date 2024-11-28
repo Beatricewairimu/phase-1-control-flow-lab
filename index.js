8d4c00f44aae1b3a73fd4d5e905c6cb2cb35151e
beatrice <wairimubeatrice209@gmail.com>
2024-11-28 23:22:39 +0300

    Done

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-function scuberGreetingForFeet(someValue) {
-  if (someValue <= 400) {
-    return "This one is on me!";
-  } else if (someValue > 400 && someValue <= 2000) {
-    return "That will be twenty bucks."; // Updated to match test expectation
-  } else if (someValue > 2000 && someValue <= 2500) {
-    return "I will gladly take your thirty bucks.";
-  } else {
-    return "No can do.";
+function scuberGreetingForFeet(someValue){
+  if(someValue <=400){
+    return('This one is on me!')
+  }else if(someValue<=2000){
+    return('That will be twenty bucks.')
+  }else if(someValue<=2500){
+    return('I will gladly take your thirty bucks.')
+  }else{
+    return('No can do.')
   }
 }
+function ternaryCheckCity(city){
+if(city=="NYC")
+return('Ok, sounds good.')
+else return('No go.')
+}
 
+function switchOnCharmFromTip(tip){
+switch (tip) {
+  case "generous":
+    return('Thank you so much.')
+    break;
+  case "not as generous":
+    return('Thank you.')
+    break;
+  default:
+    return("Bye.")
+    break;
+}
 
-
-  // Write your code here!
-  function ternaryCheckCity(city) {
-    return city === "NYC" ? "Ok, sounds good." : "No go.";
-  }
-  
-
-
-
-  // Write your code here!
-  function switchOnCharmFromTip(tip) {
-    switch (tip) {
-      case "generous":
-        return "Thank you so much.";
-      case "not as generous":
-        return "Thank you.";
-      default:
-        return "Bye.";
-    }
-  }
-  
-  
+}
